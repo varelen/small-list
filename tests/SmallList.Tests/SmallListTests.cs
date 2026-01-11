4c50582ed4500ddf3fd767d617081cf5b163f47e
varelen <varelen@proton.me>
2026-01-11 15:25:39 +0100

    feat(SmallList.Tests): Add first unit tests for 'SmallList'

---
+++ tests/SmallList.Tests/SmallListTests.cs
@@ -0,0 +1,275 @@
+// SPDX-FileCopyrightText: 2026 varelen
+// 
+// SPDX-License-Identifier: MIT
+
+using System.Runtime.CompilerServices;
+
+using Varelen.SmallList;
+
+namespace SmallList.Tests;
+
+public class SmallListTests
+{
+    [Theory]
+    [InlineData(new int[] { }, 0)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 10)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 36)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 55)]
+    public void ForEach_DirectType_MatchesExpectedSum(int[] expectedArray, int expectedSum)
+    {
+        // Arrange
+        var smallList = new SmallList<int>(expectedArray);
+
+        int sum = 0;
+
+        // Act
+        foreach (int item in smallList)
+        {
+            sum += item;
+        }
+
+        // Assert
+        Assert.Equal(expectedSum, sum);
+    }
+
+    [Theory]
+    [InlineData(new int[] { }, 0)]
+    [InlineData(new int[] { 1 }, 1)]
+    [InlineData(new int[] { 1, 2 }, 3)]
+    [InlineData(new int[] { 1, 2, 3}, 6)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 10)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 36)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 55)]
+    public void ForEach_IEnumerable_MatchesExpectedSum(int[] expectedArray, int expectedSum)
+    {
+        // Arrange
+        var smallList = new SmallList<int>(expectedArray);
+
+        // Act
+        int sum = Sum(smallList);
+
+        // Assert
+        Assert.Equal(expectedSum, sum);
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static int Sum(IEnumerable<int> enumerable)
+        {
+            int sum = 0;
+            foreach (int item in enumerable)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+
+    [Theory]
+    [InlineData(new int[] { 100, 200, 300 })]
+    [InlineData(new int[] { 100, 200, 300, 400, 500, 600, 700, 800 })]
+    [InlineData(new int[] { 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000 })]
+    public void CopyTo_GivenArray_CorrectlyCopies(int[] expectedArray)
+    {
+        // Arrange
+        var smallList = new SmallList<int>(expectedArray);
+
+        var actualArray = new int[expectedArray.Length];
+
+        // Act
+        smallList.CopyTo(actualArray, 0);
+
+        // Assert
+        for (int i = 0; i < expectedArray.Length; i++)
+        {
+            Assert.Equal(expectedArray[i], actualArray[i]);
+        }
+    }
+
+    [Fact]
+    public void Constructor_ZeroToFourArguments_MacthesExpectedItems()
+    {
+        // Arrange & Act
+        var empy = new SmallList<int>();
+        var one = new SmallList<int>(1);
+        var two = new SmallList<int>(1, 2);
+        var three = new SmallList<int>(1, 2, 3);
+        var four = new SmallList<int>(1, 2, 3, 4);
+
+        // Assert
+        AssertList(empy, []);
+        AssertList(one, [1]);
+        AssertList(two, [1, 2]);
+        AssertList(three, [1, 2, 3]);
+        AssertList(four, [1, 2, 3, 4]);
+
+        static void AssertList(in SmallList<int> smallList, int[] expectedItems)
+        {
+            Assert.Equal(expectedItems.Length, smallList.Count);
+
+            for (int i = 0; i < expectedItems.Length; i++)
+            {
+                Assert.Equal(expectedItems[i], smallList[i]);
+            }
+        }
+    }
+
+    [Fact]
+    public void Add_MultipleItems_MatchesExpectedItems()
+    {
+        // Arrange
+        var smallList = new SmallList<int>
+        {
+            // Act (calls 'Add')
+            100,
+            200,
+            300
+        };
+
+        // Assert
+        Assert.Equal(3, smallList.Count);
+        Assert.Equal(100, smallList[0]);
+        Assert.Equal(200, smallList[1]);
+        Assert.Equal(300, smallList[2]);
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(4, 4)]
+    [InlineData(5, 8)]
+    [InlineData(9, 16)]
+    public void Add_ManyItemsToForceGrowing_MatchesExpectedCapacity(int count, int expectedCapacity)
+    {
+        // Arrange
+        var smallList = new SmallList<int>();
+
+        // Act
+        for (int i = 0; i < count; i++)
+        {
+            smallList.Add(i);
+        }
+
+        // Assert
+        Assert.Equal(count, smallList.Count);
+        Assert.Equal(expectedCapacity, smallList.Capacity);
+    }
+
+    [Fact]
+    public void Remove_AllInlined_CorrectlyRemovesItems()
+    {
+        // Arrange
+        var smallList = new SmallList<int>(100, 200, 300, 400);
+
+        // Act
+        bool removedExisting = smallList.Remove(200);
+        bool removedNonExisting = smallList.Remove(200);
+
+        // Assert
+        Assert.Equal(3, smallList.Count);
+        Assert.True(removedExisting);
+        Assert.False(removedNonExisting);
+        Assert.Equal(100, smallList[0]);
+        Assert.Equal(300, smallList[1]);
+        Assert.Equal(400, smallList[2]);
+    }
+
+    [Fact]
+    public void Remove_MoreThanInlinedItemsCount_CorrectlyRemovesItems()
+    {
+        // Arrange
+        var smallList = new SmallList<int>([100, 200, 300, 400, 500, 600, 700]);
+
+        // Act
+        bool removedExisting = smallList.Remove(600);
+        bool removedNonExisting = smallList.Remove(1000);
+
+        // Assert
+        Assert.Equal(6, smallList.Count);
+        Assert.True(removedExisting);
+        Assert.False(removedNonExisting);
+        Assert.Equal(100, smallList[0]);
+        Assert.Equal(200, smallList[1]);
+        Assert.Equal(300, smallList[2]);
+        Assert.Equal(400, smallList[3]);
+        Assert.Equal(500, smallList[4]);
+        Assert.Equal(700, smallList[5]);
+    }
+
+    [Fact]
+    public void Indexer_MultipleAccessesAllInlined_ReturnsExpectedItems()
+    {
+        // Arrange
+        var smallList = new SmallList<int>(100, 200, 300, 400);
+
+        // Act
+        var first = smallList[0];
+        var second = smallList[1];
+        var third = smallList[2];
+        var fourth = smallList[3];
+
+        smallList[1] = 1000;
+        var secondAfter = smallList[1];
+
+        // Assert
+        Assert.Equal(4, smallList.Count);
+        Assert.Equal(100, first);
+        Assert.Equal(200, second);
+        Assert.Equal(300, third);
+        Assert.Equal(400, fourth);
+        Assert.Equal(1000, secondAfter);
+    }
+
+    [Fact]
+    public void Indexer_MultipleAccessesExceedingInlinedItemsCount_ReturnsExpectedItems()
+    {
+        // Arrange
+        var smallList = new SmallList<int>([100, 200, 300, 400, 500, 600]);
+
+        // Act
+        var first = smallList[0];
+        var second = smallList[1];
+        var third = smallList[2];
+        var fourth = smallList[3];
+        var five = smallList[4];
+        var six = smallList[5];
+
+        smallList[5] = 1000;
+        var sixAfter = smallList[5];
+
+        // Assert
+        Assert.Equal(6, smallList.Count);
+        Assert.Equal(100, first);
+        Assert.Equal(200, second);
+        Assert.Equal(300, third);
+        Assert.Equal(400, fourth);
+        Assert.Equal(500, five);
+        Assert.Equal(600, six);
+        Assert.Equal(1000, sixAfter);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 2, true)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 4, true)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, -1, false)]
+    [InlineData(new int[] { 1, 2, 3, 4 }, 100, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, 5, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, 6, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, 7, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, -1, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 8, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9, true)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 11, false)]
+    [InlineData(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, -1, false)]
+    public void Contains_MatchesExpectedContains(int[] items, int number, bool expectedContains)
+    {
+        // Arrange
+        var smallList = new SmallList<int>(items);
+
+        // Act
+        var containsNumber = smallList.Contains(number);
+
+        // Assert
+        Assert.Equal(items.Length, smallList.Count);
+        Assert.Equal(expectedContains, containsNumber);
+    }
+}
